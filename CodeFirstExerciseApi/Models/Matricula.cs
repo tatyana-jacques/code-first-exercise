e723cf32fcf747b65060781c20dd582db479bb01
Tatyana Jacques <taty.aj@gmail.com>
2022-12-02 19:12:11 -0300

    Primeiro exercicio.

---
+++ CodeFirstExerciseApi/Models/Matricula.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstExerciseApi.Models
+{
+    public class Matricula
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Turma")]
+        public int IdTurma { get; set; }
+
+        [ForeignKey("Curso")]
+
+        public int IdAluno { get; set; }
+
+        public DateTime? DataMatricula { get; set; }
+
+        public Turma Turma { get; set; }
+
+        public Aluno Aluno { get; set; }
+    }
+}
